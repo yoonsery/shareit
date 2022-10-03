0bf0eac837eb596e0fa49bbd6e2895c6bcf1505f
sery <yunsel2@gmail.com>
2022-10-03 11:22:32 +0900

    feat: Add length store

---
+++ src/store/length.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialType = {
+  length: 3,
+};
+
+const lengthSlice = createSlice({
+  name: 'length',
+  initialState: initialType,
+  reducers: {
+    get5More(state) {
+      state.length = state.length + 5;
+    },
+  },
+});
+
+export const lengthActions = lengthSlice.actions;
+export default lengthSlice.reducer;
