9308bfb7185ad5e79c7547e841b4e63a8434e125
sery <yunsel2@gmail.com>
2022-10-04 22:58:57 +0900

    style: Finish styling formAgree

--- src/components/Form/formAgree/FormAgree.jsx
+++ src/components/Form/formAgree/FormAgree.jsx
@@ -3,19 +3,23 @@ import styles from './FormAgree.module.css';
 const FormAgree = () => {
   return (
     <div className={styles.agree}>
-      <input
-        className={styles.checkBox}
-        type="checkbox"
-        name="terms"
-        value="agree"
-      />
-      <span className={styles.agreeText}>구매후기 약관 동의 (필수)</span>
+      <label className={styles.checkContainer}>
+        <input
+          className={styles.checkBox}
+          type="checkbox"
+          name="terms"
+          value="agree"
+        />
+        <span className={styles.checkmark}></span>
+      </label>
+
+      <div className={styles.agreeText}>구매후기 약관 동의 (필수)</div>
       <button type="button" className={styles.button}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          strokeWidth={1.5}
+          strokeWidth={2.5}
           stroke="currentColor"
           className="w-6 h-6"
         >
